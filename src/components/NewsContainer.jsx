0c07583cd654910abdddc348b1c4244b626af54e
AdityaEvans7 <adityaevans13@gmail.com>
2025-12-28 01:39:51 +0530

    fix file backend api

--- src/components/NewsContainer.jsx
+++ src/components/NewsContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import SearchBar from "./SearchBar";
+import SearchBar from "./Searchbar";
 
 const NewsContainer = () => {
   const [newsItems, setNewsItems] = useState([]);
